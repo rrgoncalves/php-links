6636218ea709a16a2b4e8d3222c8e388548dacc5
Rafael Rodrigues Gonçalves <rrgoncalves@gmail.com>
2019-04-25 12:30:02 -0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -95,7 +95,7 @@
                 </div>
 
                 <div class="links">
-                    Clean Example #88                  <!-- Update here -->
+                    Clean Example #89                  <!-- Update here -->
                     <br>
                 </div>
             </div>
